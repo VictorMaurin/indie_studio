bee48b5c25817fa28dacf7a555b920282b62b714
victor <victor.maurin@epitech.eu>
2020-06-09 16:36:48 +0200

    MOD - Bomb files

--- src/bomb/Bomb.cpp
+++ src/bomb/Bomb.cpp
@@ -1,11 +1,49 @@
 #include "Bomb.hpp"
 
-Bomb::Bomb()
+Bomb::Bomb(Core core, Player player)
 {
-
+    driver = core.getDriver();
+    device = core.getDevice();
+    smgr = core.getSmgr();
+    this->then = device->getTimer()->getTime();
+    mesh = smgr->getMesh("../../assets/bomb.obj");
+    if (!mesh) {
+        device->drop();
+        throw "device drop";
+    }
+    node = smgr->addMeshSceneNode(mesh);
+    if (node) {
+        node->setMaterialFlag(EMF_LIGHTING, false);
+        node->setMaterialTexture(0, driver->getTexture("../../assets/bombbody_BaseColor.png"));
+    }
+    // pos = player.getPos();
 }
 
 Bomb::~Bomb()
 {
+}
+
+void Bomb::explode()
+{
     
+}
+
+void Bomb::update(void)
+{
+    now = device->getTimer()->getTime();
+    if (now - then > 3) {
+        explode();
+    }
+}
+
+void Bomb::draw(void) const
+{
+}
+
+void Bomb::setPos(const irr::core::vector3df pos)
+{
+}
+
+void Bomb::getPos(void) const
+{
 }
--- src/bomb/Bomb.hpp
+++ src/bomb/Bomb.hpp
@@ -1,15 +1,36 @@
 #ifndef BOMBE_HPP
 #define BOMBE_HPP
 
-#include "../Entity/Entity.hpp"
+#include "../Core/Core.hpp"
+#include "../Player/Player.hpp"
+#include "irrlicht.h"
+using namespace irr;
+using namespace scene;
+using namespace gui;
+using namespace video;
+using namespace core;
 
-class Bomb : public Entity
+#include "../Entity/IEntity.hpp"
+
+class Bomb : public IEntity
 {
 private:
-    
-public:
-    Bomb();
+    u32 then;
+    IVideoDriver *driver;
+    IrrlichtDevice *device;
+    ISceneManager *smgr;
+    IMesh *mesh;
+    IMeshSceneNode *node;
+    vector3df pos;
+    u32 now;
+public :
+    Bomb(Core core, Player player);
     ~Bomb();
+    void update(void);
+    void draw(void) const;
+    void explode();
+    void setPos(const irr::core::vector3df pos);
+    void getPos(void) const;
 };
 
 #endif
