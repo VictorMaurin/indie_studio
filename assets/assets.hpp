db024a34cd831d83f6737ed3330ae8c1109a0e64
Paul Erny <paul@macbook-pro-de-paul.home>
2020-06-09 17:35:10 +0200

    MOD - using current_path() on windows instead of getcwd()

--- assets/assets.hpp
+++ assets/assets.hpp
@@ -8,14 +8,23 @@
 #ifndef ASSETS_H_
 #define ASSETS_H_
 
-#ifdef _WIN32
-#include <direct.h>
-#else
-#include <unistd.h>
-#endif
-
 #include <string.h>
 
+#ifdef _WIN32
+#include <direct.h>
+#include <filesystem>
+#define ASSETS_PATH(x) ({                           \
+    char a[255] = std::filesystem::current_path();  \
+    if (!strstr(a, "OOP_indie_studio_2019"))        \
+        strcat(a, "/OOP_indie_studio_2019");        \
+    strcat(a, "/assets/");                          \
+    strcat(a, x);                                   \
+    a;                                              \
+})
+
+#else
+
+#include <unistd.h>
 #define ASSETS_PATH(x) ({                           \
     char a[255];                                    \
     getcwd(a, 255);                                 \
@@ -25,5 +34,6 @@
     strcat(a, x);                                   \
     a;                                              \
 })
+#endif
 
 #endif /* !ASSETS_H_ */
