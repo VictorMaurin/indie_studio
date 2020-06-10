fdc1fd1571f08308c27841e1c4aee80f22fc9512
Paul Erny <paul@macbook-pro-de-paul.home>
2020-06-10 13:55:54 +0200

    MOD - findAsset fonction path formating is now compatible with windows

--- assets/find_asset.cpp
+++ assets/find_asset.cpp
@@ -11,18 +11,26 @@
 #ifdef _WIN32
 #include <direct.h>
 #define GetCurrentDir _getcwd
+#define NEXT_FOLDER "\"
 #else
 #include <unistd.h>
 #define GetCurrentDir getcwd
+#define NEXT_FOLDER "/"
 #endif
  
 std::string findAsset( std::string asset ) {
     char buff[FILENAME_MAX];
     GetCurrentDir( buff, FILENAME_MAX );
     std::string current_working_dir(buff);
-    if (current_working_dir.find("OOP_indie_studio_2019") == std::string::npos)
-        current_working_dir += "/OOP_indie_studio_2019";
-    current_working_dir += "/assets/";
+    if (current_working_dir.find("OOP_indie_studio_2019") == std::string::npos ||
+        current_working_dir.find("indie") == std::string::npos ||
+        current_working_dir.find("bomberman") == std::string::npos) {
+        current_working_dir += NEXT_FOLDER;
+        current_working_dir += "OOP_indie_studio_2019";
+    }
+    current_working_dir += NEXT_FOLDER;
+    current_working_dir += "assets";
+    current_working_dir += NEXT_FOLDER;
     current_working_dir += asset;
     return current_working_dir;
 }
