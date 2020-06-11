1c724c6fccf63a41341fed2da8b67f07139bd160
Paul Erny <paul@macbook-pro-de-paul.home>
2020-06-11 11:59:11 +0200

    MOD - core : fps limitation fixed

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -35,14 +35,14 @@ void Core::init()
     set_menu();
     
     //tmp light source
-    ILightSceneNode* light = smgr->addLightSceneNode( 0, vector3df(0.0f,30.0f,-40.0f), SColorf(1.0f,1.0f,1.0f,1.0f), 500.0f );
+    ILightSceneNode* light = smgr->addLightSceneNode( 0, vector3df(0.0f,50.0f,2.0f), SColorf(1.0f,1.0f,1.0f,1.0f), 30.0f );
     //tmp camera
-    smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,0,0));
+    smgr->addCameraSceneNode(0, vector3df(0,11,-1.5), vector3df(0,0,0));
 
     std::cout << "balalalal: " << this << "/" << std::endl;
-    //entities.push_back(std::make_shared<Mesh>("crate3.obj", "crate3.png", smgr, driver, device));//tmp
     entities.push_back(std::make_shared<Player>("Bomberman.MD3", "BlackBombermanTextures.png", this, smgr, driver, device, joystickInfo, eventReceiver, irr::KEY_KEY_Z, irr::KEY_KEY_S, irr::KEY_KEY_Q, irr::KEY_KEY_D));
 
+    this->map = new Map(entities, 19, 13, smgr, driver, device);
 }
 
 const std::vector<std::shared_ptr<IEntity>> &Core::getEntities() const
@@ -82,19 +82,23 @@ void Core::set_game()
 
 void Core::run()
 {
+    std::wstring fpsStr;
+    std::clock_t totalFpsTime;
     std::clock_t frameBgnTime;
     std::clock_t frameEndTime;
     double deltaTime = 0.0;
+    double totalFrameTime = 0.0;
     size_t fps = 60;
-    double frameTime = 1 / fps;
+    double frameTime = 1.0f / (float)fps;
     double remainingTime = 0.0;
     size_t updates = 0;
+    dimension2du windowSize = driver->getCurrentRenderTargetSize();
 
     while (device->run())
     {
+        totalFpsTime = std::clock();
         frameBgnTime = std::clock();
 
-        // do events
         //update
         for (int i = 0; i < entities.size(); i++)
         {
@@ -109,13 +113,13 @@ void Core::run()
 
         frameEndTime = std::clock();
 
-        deltaTime = frameEndTime - frameBgnTime;
+        deltaTime = (frameEndTime - frameBgnTime) / (double)CLOCKS_PER_SEC;
 
         //if the remaining time is > to the time of 1 frame, keep updating the game
         // this fixes the framerate at $fps frames per sec without using sleep() to prevent lags at low fps
-        remainingTime = abs((deltaTime / (double)CLOCKS_PER_SEC) - (frameTime / (double)CLOCKS_PER_SEC));
+        remainingTime = frameTime - deltaTime;
         updates = 0;
-        while (frameTime > remainingTime)
+        while (deltaTime < remainingTime)
         {
             frameBgnTime = std::clock();
             if (updates < 5)
@@ -124,10 +128,14 @@ void Core::run()
                 for (int i = 0; i < entities.size(); i++)
                     entities[i]->update();
             }
-            deltaTime = std::clock() - frameBgnTime;
-            remainingTime -= deltaTime * (double)CLOCKS_PER_SEC;
+            deltaTime = (std::clock() - frameBgnTime) / (double)CLOCKS_PER_SEC;
+            remainingTime -= deltaTime;
         }
-
+        // std::cout << totalFrameTime << " " << remainingTime << std::endl;
+        totalFrameTime = (std::clock() - totalFpsTime) / (double)CLOCKS_PER_SEC;
+        // std::cout << remainingTime << " " << 1.0f / totalFrameTime << std::endl;
+        fpsStr = std::to_wstring((int)(1.0f / totalFrameTime));
+        guienv->addStaticText(fpsStr.c_str(), irr::core::rect<irr::s32>(10, 10, 30, 20), true);
         if (this->eventReceiver->IsKeyDown(irr::KEY_KEY_E) || this->eventReceiver->IsKeyDown(irr::KEY_ESCAPE))
             device->closeDevice();
     }
